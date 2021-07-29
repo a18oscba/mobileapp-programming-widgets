0d3cb5ab59b80b241d25e0d40efd53b3ad83a90c
Oscar <a18oscba@student.his.se>
2021-07-29 18:00:29 +0200

    Widgets
    As the first step, I added the widgets I wanted

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
